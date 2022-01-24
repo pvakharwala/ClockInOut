f2035757afd849dd8aa569ed8fc98d01e254d206
Parantap Vakharwala <13331698+pvakharwala@users.noreply.github.com>
2022-01-23 19:38:06 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ClockInOut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.clockOutTextbox = new System.Windows.Forms.TextBox();
+            this.clockInTextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.punchBtn = new System.Windows.Forms.Button();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.punchLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(403, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.BackColor = System.Drawing.SystemColors.Control;
+            this.nameTextbox.ForeColor = System.Drawing.Color.Black;
+            this.nameTextbox.Location = new System.Drawing.Point(407, 61);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(160, 29);
+            this.nameTextbox.TabIndex = 5;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.BackColor = System.Drawing.SystemColors.Control;
+            this.passwordTextbox.ForeColor = System.Drawing.Color.Black;
+            this.passwordTextbox.Location = new System.Drawing.Point(407, 137);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.PasswordChar = '*';
+            this.passwordTextbox.Size = new System.Drawing.Size(160, 29);
+            this.passwordTextbox.TabIndex = 6;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(606, 94);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(149, 40);
+            this.loginBtn.TabIndex = 7;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(158, 33);
+            this.dateTimePicker.MaxDate = new System.DateTime(2050, 12, 31, 0, 0, 0, 0);
+            this.dateTimePicker.MinDate = new System.DateTime(2021, 12, 30, 0, 0, 0, 0);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(200, 29);
+            this.dateTimePicker.TabIndex = 1;
+            this.dateTimePicker.Value = new System.DateTime(2022, 1, 23, 0, 0, 0, 0);
+            // 
+            // clockOutTextbox
+            // 
+            this.clockOutTextbox.ForeColor = System.Drawing.Color.Black;
+            this.clockOutTextbox.Location = new System.Drawing.Point(158, 127);
+            this.clockOutTextbox.Name = "clockOutTextbox";
+            this.clockOutTextbox.Size = new System.Drawing.Size(200, 29);
+            this.clockOutTextbox.TabIndex = 3;
+            // 
+            // clockInTextbox
+            // 
+            this.clockInTextbox.ForeColor = System.Drawing.Color.Black;
+            this.clockInTextbox.Location = new System.Drawing.Point(158, 80);
+            this.clockInTextbox.Name = "clockInTextbox";
+            this.clockInTextbox.Size = new System.Drawing.Size(200, 29);
+            this.clockInTextbox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Clock Out";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 83);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Clock In";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.Location = new System.Drawing.Point(51, 37);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Date";
+            // 
+            // punchBtn
+            // 
+            this.punchBtn.Location = new System.Drawing.Point(180, 178);
+            this.punchBtn.Name = "punchBtn";
+            this.punchBtn.Size = new System.Drawing.Size(149, 40);
+            this.punchBtn.TabIndex = 4;
+            this.punchBtn.Text = "Punch In";
+            this.punchBtn.UseVisualStyleBackColor = true;
+            this.punchBtn.Click += new System.EventHandler(this.punchBtn_Click);
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.loginLabel.Location = new System.Drawing.Point(407, 178);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(0, 24);
+            this.loginLabel.TabIndex = 14;
+            // 
+            // punchLabel
+            // 
+            this.punchLabel.AutoSize = true;
+            this.punchLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.punchLabel.Location = new System.Drawing.Point(176, 221);
+            this.punchLabel.Name = "punchLabel";
+            this.punchLabel.Size = new System.Drawing.Size(0, 24);
+            this.punchLabel.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(41, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "24 Hr Format";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(784, 269);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.punchLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.punchBtn);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.clockOutTextbox);
+            this.Controls.Add(this.clockInTextbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.nameTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wireless Warehouse";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.TextBox clockOutTextbox;
+        private System.Windows.Forms.TextBox clockInTextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button punchBtn;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label punchLabel;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using ClockInOut.Properties;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4.Data;
+
+namespace ClockInOut
+{
+    public partial class Form1 : Form
+    {
+
+        public int login_state = 0;
+        public int valid = 0;
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string ApplicationName = "Timesheet";
+        static readonly string SpreadsheetId = "1LkE2sVnjBM7bDo-C2pVa2k8INrrMpTUlgzpppmVRzQE";
+        static string name; 
+        static SheetsService service;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            
+            string[] tempid = Settings.Default["id"].ToString().Split(',');
+            int[] ids = Array.ConvertAll(tempid, int.Parse);
+            if (login_state == 0)
+            {
+                int pass;
+                if (Int32.TryParse(passwordTextbox.Text.ToString(), out pass))
+                {
+                    for (int i = 0; i < ids.Length; i++)
+                    {
+                        if (ids[i] == pass)
+                        {
+                            valid = 1;
+                        }
+                    }
+                    if (valid == 1)
+                    {
+                        nameTextbox.ReadOnly = true;
+                        passwordTextbox.ReadOnly = true;
+                        loginBtn.Text = "Logout";
+                        login_state = 1;
+                        loginLabel.ForeColor = System.Drawing.Color.Green;
+                        loginLabel.Text = "Login Successful";
+                    }
+                    else
+                    {
+                        valid = 0;
+                        loginLabel.ForeColor = System.Drawing.Color.Red;
+                        loginLabel.Text = "Login Unsuccessful";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Entered password is not a number!", "Inavlid Data");
+                }
+                
+                
+            }
+            else
+            {
+                nameTextbox.ReadOnly = false;
+                passwordTextbox.ReadOnly = false;
+                loginBtn.Text = "Login";
+                login_state = 0;
+                valid = 0;
+                loginLabel.Text = "Logout Successful";
+            }
+        }
+
+        private void punchBtn_Click(object sender, EventArgs e)
+        {
+            if (valid==1)
+            {
+               name = nameTextbox.Text.ToString();
+                GoogleCredential credential;
+                using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+                {
+                    credential = GoogleCredential.FromStream(stream)
+                        .CreateScoped(Scopes);
+                }
+                service = new SheetsService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                });
+                double clockin;
+                double clockout;
+                if (Double.TryParse(clockInTextbox.Text.ToString(), out clockin))
+                {
+                    if (Double.TryParse(clockOutTextbox.Text.ToString(), out clockout))
+                    {
+                        double hours = clockout - clockin;
+                        if (hours>=0)
+                        {
+                            try
+                            {
+                                var range = $"{name}!A:C";
+                                var valueRange = new ValueRange();
+
+                                var oblist = new List<object>() { dateTimePicker.Text.ToString(), clockin.ToString(), clockout.ToString(), hours.ToString() };
+                                valueRange.Values = new List<IList<object>> { oblist };
+                                var appendRequest = service.Spreadsheets.Values.Append(valueRange, SpreadsheetId, range);
+                                appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+                                var appendReponse = appendRequest.Execute();
+                                punchLabel.ForeColor = System.Drawing.Color.Green;
+                                punchLabel.Text = hours.ToString() + " Hour/s Submitted!";
+                            }
+                            catch (Exception ex)
+                            {
+
+                                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Clock Out cannot be before Clock In", "Inavlid Data");
+                        }
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Entered Clock Out is not a number!", "Inavlid Data");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Entered Clock In is not a number!", "Inavlid Data");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Login required", "Error!");
+            }
+
+        }
+    }
+}
